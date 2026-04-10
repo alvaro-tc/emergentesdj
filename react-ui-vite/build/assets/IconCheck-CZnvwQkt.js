1584f078e079f7456afa0c76dfac5bc009c903f1
alvaro-tc <alvarotorrezc@gmail.com>
2026-04-10 18:02:36 -0400

    Solucionado problema redux

--- react-ui-vite/build/assets/IconCheck-CZnvwQkt.js
+++
@@ -1,6 +0,0 @@
-import{k as e}from"./index-CpbR95Vl.js";/**
- * @license @tabler/icons-react v3.41.1 - MIT
- *
- * This source code is licensed under the MIT license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M5 12l5 5l10 -10",key:"svg-0"}]],t=e("outline","check","Check",o);export{t as I};
