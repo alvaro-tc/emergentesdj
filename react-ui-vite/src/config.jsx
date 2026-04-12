47af5fc54fa73f7e5a5932e96266f2571609b826
Mario Torrez <mtorrezc@hotmail.com>
2026-04-11 22:14:40 -0400

    Modificado configuracion para .env

--- react-ui-vite/src/config.jsx
+++ react-ui-vite/src/config.jsx
@@ -1,5 +1,7 @@
 let BACKEND_SERVER = null;
-if (process.env.REACT_APP_BACKEND_SERVER) {
+if (import.meta.env && import.meta.env.VITE_API_URL) {
+  BACKEND_SERVER = import.meta.env.VITE_API_URL;
+} else if (typeof process !== 'undefined' && process.env && process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
   BACKEND_SERVER = "http://localhost:5000/api/";
